9bfe785c62c7972d990c31bcb1547d1964a00bf7
wzx <wangzixu98@icloud.com>
2021-12-22 23:05:04 +0800

    凸优化实验代码修改

--- 作业和实验/凸优化/LP_QP_SDP.m
+++ 作业和实验/凸优化/LP_QP_SDP.m
@@ -1,73 +1,73 @@
 clear;
 clc;
 %% LP
-% % define coefficients
-% c = [4, -3, -6, 1]';
-% A = [3, 5, -6;
-%     2, -1, 3;
-%     7, -4, 8;
-%     -5, 2, 3];
-% Ix = [-100, -100, -100, -100]';
-% ux = [100, 100, 100, 100]';
-% Iy = [-100, -100, -100]';
-% uy = [100, 100, 100]';
-% % define variables
-% x = sdpvar(4, 1);
-% y = sdpvar(3, 1);
-% % objective
-% obj = c' * x;
-% % constrains
-% cons = [x == A * y, Ix <= x <= ux, Iy <= y <= uy];
-% % options
-% options = sdpsettings('solver', 'sedumi', 'verbose', 0);
-% % optimize
-% optimize(cons, obj, options);
-% % solution
-% x_sol = value(x);
-% y_sol = value(y);
-% % disp
-% disp('LP: ');
-% disp('optimal x: ');
-% disp(x_sol);
-% disp('optimal y: ');
-% disp(y_sol);
+% define coefficients
+c = [4, -3, -6, 1]';
+A = [3, 5, -6;
+    2, -1, 3;
+    7, -4, 8;
+    -5, 2, 3];
+Ix = [-100, -100, -100, -100]';
+ux = [100, 100, 100, 100]';
+Iy = [-100, -100, -100]';
+uy = [100, 100, 100]';
+% define variables
+x = sdpvar(4, 1);
+y = sdpvar(3, 1);
+% objective
+obj = c' * x;
+% constrains
+cons = [x == A * y, Ix <= x <= ux, Iy <= y <= uy];
+% options
+options = sdpsettings('solver', 'sedumi', 'verbose', 0);
+% optimize
+optimize(cons, obj, options);
+% solution
+x_sol = value(x);
+y_sol = value(y);
+% disp
+disp('LP: ');
+disp('optimal x: ');
+disp(x_sol);
+disp('optimal y: ');
+disp(y_sol);
 %% QP
-% % define coefficients
-% Q = [2, 5, 3;
-%     4, 6, 8;
-%     1, 0, 7];
-% A = [5, 2;
-%     -1, -3;
-%     6, 8];
-% Q1 = [1, 2, 3;
-%     3, 6, 9;
-%     4, 2, 5];
-% Q2 = [7, 2, 1;
-%     9, 3, 5;
-%     6, 1, 4];
-% b1 = 100;
-% b2 = 100;
-% u = [100, -100]';
-% % define variables
-% x = sdpvar(3, 1);
-% y = sdpvar(2, 1);
-% % objective
-% obj = x' * Q * x;
-% % constrains
-% cons = [x == A * y, x' * Q1 * x <= b1, x' * Q2 * x <= b2, y <= u];
-% % options
-% options = sdpsettings('solver', '', 'verbose', 0);
-% % optimize
-% optimize(cons, obj, options);
-% % solution
-% x_sol = value(x);
-% y_sol = value(y);
-% % disp
-% disp('QP: ');
-% disp('optimal x: ');
-% disp(x_sol);
-% disp('optimal y: ');
-% disp(y_sol);
+% define coefficients
+Q = [2, 5, 3;
+    4, 6, 8;
+    1, 0, 7];
+A = [5, 2;
+    -1, -3;
+    6, 8];
+Q1 = [1, 2, 4;
+    2, 6, 2;
+    4, 2, 5];
+Q2 = [7, 9, 1;
+    9, 3, 5;
+    1, 5, 4];
+b1 = 100;
+b2 = 100;
+u = [100, -100]';
+% define variables
+x = sdpvar(3, 1);
+y = sdpvar(2, 1);
+% objective
+obj = x' * Q * x;
+% constrains
+cons = [x == A * y, x' * Q1 * x <= b1, x' * Q2 * x <= b2, y <= u];
+% options
+options = sdpsettings('solver', '', 'verbose', 0);
+% optimize
+optimize(cons, obj, options);
+% solution
+x_sol = value(x);
+y_sol = value(y);
+% disp
+disp('QP: ');
+disp('optimal x: ');
+disp(x_sol);
+disp('optimal y: ');
+disp(y_sol);
 %% SDP
 % define coefficients
 C = diag([5, 6, 9, 3]);
@@ -79,11 +79,11 @@ E = [5, 3, -2, 4;
 b = [100, 100, 100];
 f = [100, 10];
 % define variables
-X = sdpvar(4, 4, 'symmetric');
+X = sdpvar(4, 4, 'symmetric', 'real');
 % objective
 obj = trace(C * X);
 % constrains
-cons = [X >= 0];
+cons = [X >= zeros(4, 4), X' == X];
 for k = 1: 3
     cons = cons + [trace(diag(A(k, :)) * X) <= b(k)];
 end
